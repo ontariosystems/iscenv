1b12d7466df465759e0e2a67ca287bb9d3afa63a
Lucas Heinlen <lucas.heinlen@gmail.com>
2016-08-02 15:52:02 -0400

    Revendor

--- vendor/github.com/spf13/viper/viper.go
+++ vendor/github.com/spf13/viper/viper.go
@@ -31,12 +31,11 @@ import (
 	"strings"
 	"time"
 
-	"github.com/kr/pretty"
+	"github.com/fsnotify/fsnotify"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cast"
 	jww "github.com/spf13/jwalterweatherman"
 	"github.com/spf13/pflag"
-	"gopkg.in/fsnotify.v1"
 )
 
 var v *Viper
@@ -540,7 +539,7 @@ func (v *Viper) GetString(key string) string {
 	return cast.ToString(v.Get(key))
 }
 
-// Returns the value associated with the key asa boolean
+// Returns the value associated with the key as a boolean
 func GetBool(key string) bool { return v.GetBool(key) }
 func (v *Viper) GetBool(key string) bool {
 	return cast.ToBool(v.Get(key))
@@ -1284,17 +1283,11 @@ func (v *Viper) findConfigFile() (string, error) {
 func Debug() { v.Debug() }
 func (v *Viper) Debug() {
 	fmt.Println("Aliases:")
-	pretty.Println(v.aliases)
-	fmt.Println("Override:")
-	pretty.Println(v.override)
-	fmt.Println("PFlags")
-	pretty.Println(v.pflags)
-	fmt.Println("Env:")
-	pretty.Println(v.env)
-	fmt.Println("Key/Value Store:")
-	pretty.Println(v.kvstore)
-	fmt.Println("Config:")
-	pretty.Println(v.config)
-	fmt.Println("Defaults:")
-	pretty.Println(v.defaults)
+	fmt.Printf("Aliases:\n%#v\n", v.aliases)
+	fmt.Printf("Override:\n%#v\n", v.override)
+	fmt.Printf("PFlags:\n%#v\n", v.pflags)
+	fmt.Printf("Env:\n%#v\n", v.env)
+	fmt.Printf("Key/Value Store:\n%#v\n", v.kvstore)
+	fmt.Printf("Config:\n%#v\n", v.config)
+	fmt.Printf("Defaults:\n%#v\n", v.defaults)
 }
